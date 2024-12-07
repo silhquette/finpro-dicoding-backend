51ce5e82acd519b0e61c74f0058ef9c184d0dd9d
Silhquette <muhammadqiyam03@gmail.com>
2024-12-07 19:15:48 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express')
+
+const PORT = 8000
+const HOST = '0.0.0.0'
+
+const app = express()
+/*
+[ROUTE] '/'
+[Response] => Hello world!
+ */
+app.get('/', (req, res) => {
+    res.send('Hello world!\n')
+})
+
+app.get('/me', (req, res) => {
+    res.send('muhammadqiyam')
+})
+
+/*
+Optional [ROUTE] '/me'
+[Response] => Dicoding username.
+*/
+
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
